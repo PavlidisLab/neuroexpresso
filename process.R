8bfdd7f0b7f0b7cfbc665fd6753e4ff66787a7c7
OganM <ogan.mancarcii@gmail.com>
2015-09-21 15:30:41 -0700

    unnecesarry lines removed

--- process.R
+++ process.R
@@ -21,31 +21,3 @@ write.table(gene2,'Data/mouseGene2',sep=',',row.names=F)
 write.table(format(exp2,digits=3),'Data/mouseExpr2',sep=',',row.names=F,quote=F)
 
 purge()
-# mouseExpr =  fread('Data/mouseExpr')
-# mouseGene = read.csv('Data/mouseGene')
-# mouseDes = read.design('Data/meltedDesign.tsv')
-# 
-# mouseExpr2 =  fread('Data/mouseExpr2')
-# mouseGene2 = read.csv('Data/mouseGene2')
-# mouseDes2 = read.design('Data/meltedDesign2.tsv')
-# 
-# 
-# tempGene2 = mouseGene2[match(mouseGene$Gene.Symbol,mouseGene2$Gene.Symbol),]
-# tempExp2 = mouseExpr2[match(mouseGene$Gene.Symbol,mouseGene2$Gene.Symbol),]
-#     
-# tempGene1 = mouseGene[!is.na(tempGene2$Gene.Symbol),]
-# tempExp1 = mouseExpr[!is.na(tempGene2$Gene.Symbol),]
-# 
-# tempExp2 = tempExp2[!is.na(tempGene2$Gene.Symbol),]
-# tempGene2 = tempGene2[!is.na(tempGene2$Gene.Symbol),]
-# 
-# sameProbes = tempGene1$Probe[as.char(tempGene2$Probe) == as.char(tempGene1$Probe)]
-# 
-# remove = (mouseGene2$Probe %in% sameProbes)
-# 
-# mouseGene2 = mouseGene2[!remove,]
-# mouseExpr2 = mouseExpr2[!remove,]
-# write.table(mouseGene2,'Data/mouseGene22',sep=',',row.names=F)
-# write.table(mouseExpr2,'Data/mouseExpr22',sep=',',row.names=F,quote=F)
-# 
-
