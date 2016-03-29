a637749861d720106682bcaa8d766a6a423df8e8
Ogan Mancarci <ogan.mancarcii@gmail.com>
2016-03-29 16:01:42 -0700

    funding source

--- ui.R
+++ ui.R
@@ -56,60 +56,74 @@ shinyUI(fluidPage(theme = shinytheme('flatly'),
     includeCSS('www/style.css'),
     extendShinyjs(text = javaScript),
     titlePanel("NeuroExpresso"),
-    sidebarLayout(
-        sidebarPanel(
-            tags$head(tags$script('$(function () { $("#expressionPlot").click(function(e){ $("#ggvis-tooltip").hide(); }); })')),
-            inputIp("ipid"),
-            inputUserid("fingerprint"),
-            h1('About'),
-            p('This application aims to make it easier to visualize gene expression in mouse brain cell types. The data here is compiled for a project aiming to select cell type specific genes in brain.'),
-            p('It is compiled by combining data from', 
-              a(href="http://www.chibi.ubc.ca/Gemma/arrays/showArrayDesign.html?id=7", 'GPL339'),'and',
-              a(href="http://www.chibi.ubc.ca/Gemma/arrays/showArrayDesign.html?id=3", 'GPL1261')),
-            p('To see genes that are only available for GPL1261, choose that platform below. This will remove some of the samples'),
-            p('Click on data points to see their sources'),
-            a(href="https://github.com/oganm/cellTypeExpression", 'Source Code'),
-            br(),
-            a(href="https://github.com/oganm/brainCellTypeSpecificGenes", 'Project Page'),
-            br(),
-            p('Wait until the plot renders then enter a gene symbol.'),
-            fluidRow(
-                column(4,htmlOutput(outputId = 'geneSearchHtml')),
-                column(4, htmlOutput(outputId ='regionSelectHtml')),
-                column(4,selectInput(inputId = 'platform',
-                                     label = 'Select Platform',
-                                     choices = c('GPL339','GPL1261')))
-            ),
-            
-            
-            textOutput(outputId = 'didYouMean'),
-            textOutput(outputId = 'synonyms'),
-            
-            uiOutput('expressionUI'),
-            fluidRow(
-                column(2,
-                       checkboxInput(inputId = 'color',
-                                     label = 'Color?', 
-                                     value = T)),
-                column(3,
-                       radioButtons(inputId = 'ordering',
-                                    label = 'Order by',
-                                    choices = c('Cell type','A-Z'), selected = NULL, inline = FALSE, width = NULL)),
-                column(7, 
-                       htmlOutput(outputId = 'selectTree'),
-                       #htmlOutput('tree'))
-                       shinyTree("tree",search = TRUE))
-            )
+    fluidRow(column(4,wellPanel(
+        tags$head(tags$script('$(function () { $("#expressionPlot").click(function(e){ $("#ggvis-tooltip").hide(); }); })')),
+        inputIp("ipid"),
+        inputUserid("fingerprint"),
+        h1('About'),
+        p('This application aims to make it easier to visualize gene expression in mouse brain cell types. The data here is compiled for a project aiming to select cell type specific genes in brain.'),
+        p('It is compiled by combining data from', 
+          a(href="http://www.chibi.ubc.ca/Gemma/arrays/showArrayDesign.html?id=7", 'GPL339'),'and',
+          a(href="http://www.chibi.ubc.ca/Gemma/arrays/showArrayDesign.html?id=3", 'GPL1261')),
+        p('To see genes that are only available for GPL1261, choose that platform below. This will remove some of the samples'),
+        p('Click on data points to see their sources'),
+        a(href="https://github.com/oganm/cellTypeExpression", 'Source Code'),
+        br(),
+        a(href="https://github.com/oganm/brainCellTypeSpecificGenes", 'Project Page'),
+        br(),
+        p('Wait until the plot renders then enter a gene symbol.'),
+        fluidRow(
+            column(4,htmlOutput(outputId = 'geneSearchHtml')),
+            column(4, htmlOutput(outputId ='regionSelectHtml')),
+            column(4,selectInput(inputId = 'platform',
+                                 label = 'Select Platform',
+                                 choices = c('GPL339','GPL1261')))
         ),
-        mainPanel(
-            htmlOutput('warning'),
-            ggvisOutput('expressionPlot')
-        ),
+        
+        
+        textOutput(outputId = 'didYouMean'),
+        textOutput(outputId = 'synonyms'),
+        
+        uiOutput('expressionUI'),
+        fluidRow(
+            column(2,
+                   checkboxInput(inputId = 'color',
+                                 label = 'Color?', 
+                                 value = T)),
+            column(3,
+                   radioButtons(inputId = 'ordering',
+                                label = 'Order by',
+                                choices = c('Cell type','A-Z'), selected = NULL, inline = FALSE, width = NULL)),
+            column(7, 
+                   htmlOutput(outputId = 'selectTree'),
+                   #htmlOutput('tree'))
+                   shinyTree("tree",search = TRUE))
+        )
+    )),
+    column(7, 
+           htmlOutput('warning'),
+           ggvisOutput('expressionPlot'))),
+    fluidRow(column(4,
+                    wellPanel(
+                        p('Developed and maintained by',
+                          a(href="https://github.com/oganm/", 'Ogan Mancarci'), 'at',
+                          a(href="http://www.chibi.ubc.ca/faculty/paul-pavlidis/pavlidis-lab/", 'Pavlidis Lab')
+                          ),
+                        br(),
+                        p('Supported by'),
+                        a(href = 'http://neurodevnet.ca/',
+                          img(src = 'NeuroDevNet.png',height = '100')),
+                        a(href= 'http://www.nih.gov/',
+                          img(src='NIH.png', height = '100')),
+                        a(href = 'http://www.cihr-irsc.gc.ca/e/193.html',
+                          img(src = 'cihr.png', height = '100'))
+                        )
+                    )
+             )
         #fluidRow('sadasd')
         #fluidRow(#column(2,
                    #     wellPanel(
                   #          p('hello nurse')
                  #       ))
          #        )
-    )
 ))
