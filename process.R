dc9447d0b707c30d39fe19273736f11f67226eb9
Ogan Mancarci <ogan.mancarcii@gmail.com>
2016-09-14 11:01:40 -0700

    Process points to the new directory

--- process.R
+++ process.R
@@ -3,14 +3,14 @@ library(magrittr)
 #library(ogbox)
 prop = 'ShinyNames'
 mouseDes2 =
-    read.design('/home/omancarci/wholeOtto/omancarci/brainCellTypeSpecificGenes/data/meltedDesign2.tsv')
+    read.design('/home/omancarci/wholeOtto/omancarci/brainGenesManuscript/data-raw/Mouse_Cell_Type_Data/n_expressoSamples2.tsv')
 expression2 = 
-    read.exp('/home/omancarci/wholeOtto/omancarci/brainCellTypeSpecificGenes/data/finalExp2.csv')
+    read.exp('/home/omancarci/wholeOtto/omancarci/brainGenesManuscript/data-raw/Mouse_Cell_Type_Data/n_expressoExpr2.csv')
 
 mouseDes = 
-    read.design('/home/omancarci/wholeOtto/omancarci/brainCellTypeSpecificGenes/data/meltedDesign.tsv')
+    read.design('/home/omancarci/wholeOtto/omancarci/brainGenesManuscript/data-raw/Mouse_Cell_Type_Data/n_expressoSamples.tsv')
 expression = 
-    read.exp('/home/omancarci/wholeOtto/omancarci/brainCellTypeSpecificGenes/data/finalExp.csv')
+    read.exp('/home/omancarci/wholeOtto/omancarci/brainGenesManuscript/data-raw/Mouse_Cell_Type_Data/n_expressoExpr.csv')
 
 
 list[gene,exp] = sepExpr(expression)
